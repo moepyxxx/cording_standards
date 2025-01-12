b3d0869ad1797daeeac1e0e8e6837b3ececc9f94
moeko-iwai <moepyxxxxxx@gmail.com>
2025-01-12 15:37:37 +0900

    npm run develop でこけたところを確認して修正

--- app/gatsby-config.ts
+++ app/gatsby-config.ts
@@ -2,14 +2,13 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Cording Standards`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "My Cording Standards",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-postcss"],
 };
 
 export default config;
---
+++ app/gatsby-node.js
@@ -0,0 +1,9 @@
+// see: https://github.com/gatsbyjs/gatsby/issues/28657
+exports.onCreateBabelConfig = ({ actions }) => {
+  actions.setBabelPlugin({
+    name: "@babel/plugin-transform-react-jsx",
+    options: {
+      runtime: "automatic",
+    },
+  });
+};
